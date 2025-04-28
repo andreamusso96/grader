2ec08cbb7048307fa0d9328227de2bb18a5e4aca
stefanoviel <stefi.viel@gmail.com>
2025-04-28 21:51:58 +0200

    identation

--- src/types.ts
+++ src/types.ts
@@ -4,14 +4,14 @@ export type QuestionBase = {
     length: number;          // options count OR lines
   };
   
-  export type MCQ  = QuestionBase & { qType: "mcq";  options: string[] };
-  export type Open = QuestionBase & { qType: "open" };
-  
-  export type Question = MCQ | Open;
-  
-  export type Exam = {
-    id: string;
-    title: string;
-    questions: Question[];
-  };
+export type MCQ  = QuestionBase & { qType: "mcq";  options: string[] };
+export type Open = QuestionBase & { qType: "open" };
+
+export type Question = MCQ | Open;
+
+export type Exam = {
+  id: string;
+  title: string;
+  questions: Question[];
+};
   
